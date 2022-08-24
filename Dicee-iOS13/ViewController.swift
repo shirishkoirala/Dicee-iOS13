9b95caac7ed773eae2fb444352ae582a9aa7cd34
Shirish Koirala <shirishkoirala@gmail.com>
2022-08-24 12:39:42 +1000

    Swift Programming Basics Completed with DIcee App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0;
+    var rightDiceNumber = 5;
+    
+    let diceImageList =  [UIImage(named: "DiceOne"),
+                          UIImage(named: "DiceTwo"),
+                          UIImage(named: "DiceThree"),
+                          UIImage(named: "DiceFour"),
+                          UIImage(named: "DiceFive"),
+                          UIImage(named: "DiceSix")]
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceImageList.randomElement()!!
+        diceImageView2.image = diceImageList.randomElement()!!
+    
+
     }
-
-
+    
 }
 
